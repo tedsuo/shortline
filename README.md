5f02d230d5cee192fdcb2337086befa2ae1dc1dc
tedsuo <ted@radicaldesigns.org>
2011-07-22 14:08:07 -0700

    basic layout

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JOB PROCESSOR
+# JOB BOARD
 
 ## Components
 - Receiver
---
+++ job_board.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Board: require('./lib/Board.js'),
+  Process: require('./lib/Process.js'),
+  Receiver: require('./lib/Receiver.js'),
+  Queue: require('./lib/Queue.js'),
+  Pusher: require('./lib/Pusher.js'),
+  Storage: require('./lib/Storage.js')
+}
