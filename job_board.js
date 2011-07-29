f60047fbc579aa4a3f08cbb86d57657db20fdf74
cooperq <cooper@radicaldesigns.org>
2011-07-29 15:04:10 -0700

    tyring to get forever working

--- job_board.js
+++ job_board.js
@@ -1,8 +1,12 @@
-module.exports = {
-  Board: require('./lib/Board.js'),
-  Process: require('./lib/Process.js'),
-  Receiver: require('./lib/Receiver.js'),
-  Queue: require('./lib/Queue.js'),
-  Pusher: require('./lib/Pusher.js'),
-  Storage: require('./lib/Storage.js')
-}
+net = require('net');
+fs = require('fs');
+mongodb = require('mongodb');
+
+board = require('./lib/Board');
+
+var server = net.createServer(function (c) {
+  c.write('hello\r\n');
+  c.pipe(c);
+});
+server.listen(8124, 'localhost');
+console.log("Started listening on TCP/8124");
