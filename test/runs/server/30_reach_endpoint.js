02b1dc15d6e16fb11ef4fef86a1a4daa63ef805b
William Budington <bill.budington@gmail.com>
2011-11-15 00:42:29 -0800

    Fixing test: runs/server/30

--- test/runs/server/30_reach_endpoint.js
+++ test/runs/server/30_reach_endpoint.js
@@ -5,25 +5,43 @@ var express = require('express');
 var BINPATH = '../bin/jb';
 
 var endpoint = express.createServer();
+var endpoint_reached = false;
 var global_next;
 
-endpoint.get('/some/path', function(req, res){
+endpoint.post('/some/path', function(req, res){
+  endpoint_reached = true;
   global_next();
-})
+});
+
+endpoint.listen(8010);
+
+function delete_receiver_path(callback){
+  exec(BINPATH + ' delete receiver testing', function(){
+    callback();
+  });
+}
 
 async.series([
   function(next){
     global_next = next;
     console.log("Server reaches a valid endpoint");
-    exec(BINPATH + ' add receiver testing localhost -p 8010 -m test', function(err, stderr){
-      exec(BINPATH + ' add path testing somepath some/path -m test', function(err, stderr){
+    exec(BINPATH + ' add receiver testing localhost -p 8010 -m test', function(){
+      exec(BINPATH + ' add path testing somepath some/path -m test', function(){
         exec("curl -d '{}' http://localhost:8009/testing/somepath", function(err, stdout){
           assert.notEqual(stdout.indexOf("Job saved. Good job!"), -1, "Valid receivers should display a confirmation.");
           exec(BINPATH + ' remove receiver testing -m test', function(err, stderr){
-            next();
+            setTimeout(function(){
+              delete_receiver_path(function(){
+                assert.fail(false, false, 'Endpoint not reached within 6 seconds.');
+              });
+            }, 6000);
           });
         });
       });
     });
   }
-]);
+], function(err, results){
+  delete_receiver_path(function(){
+    process.exit();
+  });
+});
