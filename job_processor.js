56f94b853338a0dbbb48de994999fdc3f8f40695
William Budington <bill.budington@gmail.com>
2011-12-09 18:49:23 -0800

    Fixing bug when all jobs finish before mongo returns from refilling

--- job_processor.js
+++ job_processor.js
@@ -127,26 +127,31 @@ JobProcessor.prototype.refill_queue = function(){
   }).sort('created', 'ascending').limit(this.refill_size);
   q.execFind(function(err, jobs){
     console.log('R '+jp.receiver.name+': retreived ' + jobs.length + " jobs from mongo");
-    var job_functions = [];
+
+    var job_save_states = [];
+    var job_queues = [];
     _.each(jobs, function(job){
-      job_functions[job_functions.length] = function(done){
-        jp.queue.push(job);
-        if(job.status != "queued"){
-          job.status = "queued";
-          job.save(function(err){
-            if(err){
-              done(err);
-            } else {
-              done();
-            }
-          });
+      job_save_states.push(function(done){
+        job.status = "queued";
+        job.save(function(err){
+          if(err){
+            done(err);
+          } else {
+            done();
+          }
+        });
+      });
+      job_queues.push(function(done){
+        if(jp.concurrent_connections < jp.receiver.concurrency && jp.queue.length == 0){
+          // all queued jobs finished while fetching from mongo, and then some
+          jp.process_job(job);
         } else {
-          done();
+          jp.queue.push(job);
         }
-      };
+      });
     });
-    async.series(
-      job_functions,
+    async.parallel(
+      job_save_states,
       function(err){
         if(jobs.length < jp.refill_size){
           console.log('R '+jp.receiver.name+': state switched to open');
@@ -155,6 +160,9 @@ JobProcessor.prototype.refill_queue = function(){
           console.log('R '+jp.receiver.name+': state switched to draining');
           jp.state = "draining";
         }
+        _.each(job_queues, function(job_queue){
+          job_queue();
+        });
         jp.emit('refilling_finished');
       }
     );
