28e7a6d584c48b276fec1a59260c95873d31e5f8
William Budington <bill.budington@gmail.com>
2012-03-21 17:44:59 -0700

    Replacing curl with http lib for remaining server tests

--- test/spec/integration/server.js
+++ test/spec/integration/server.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var _ = require('underscore');
 var async = require('async');
 var exec = require('child_process').exec;
 var express = require('express');
@@ -17,6 +18,11 @@ function delete_receiver_path(callback){
 var endpoint = express.createServer();
 endpoint.listen(8010);
 
+var generic_request_options = {
+  host: 'localhost',
+  port: config.port,
+  method: 'POST'
+};
 
 describe('Server', function(){
 
@@ -44,22 +50,38 @@ describe('Server', function(){
 
   describe('False Routes', function(){
     it("should return error message for false receiver",function(next){
-      exec("curl -d '{}' http://localhost:"+config.port+"/false/somepath", function(err, stdout){
-        if(err) return next(err);
-        assert.notEqual(stdout.indexOf("No receiver by that name found."), -1, "False receivers should display an error");
-        next();
+      var options = _.extend({path: '/false/somepath'}, generic_request_options);
+      var body = "";
+      var req = http.request(options, function(res){
+        res.on('data',function(chunk){
+          body += chunk;
+        });
+        res.on('end',function(){
+          assert.notEqual(body.indexOf("No receiver by that name found."), -1, "False receivers should display an error");
+          next();
+        });
       });
+      req.write("{}");
+      req.end();
     });
     it("Server displays error for false path",function(next){
       exec(BINPATH + " add receiver somerecv test.com -m test", function(err, stdout){
-        exec("curl -d '{}' http://localhost:"+config.port+"/somerecv/blah", function(err, stdout){
-          if(err) return next(err);
-          assert.notEqual(stdout.indexOf("No path by that name found."), -1, "False paths should display an error.");
-          exec(BINPATH + " remove receiver somerecv -m test", function(err, stdout){
-            if(err) return next(err);
-            next();
+        var options = _.extend({path: '/somerecv/blah'}, generic_request_options);
+        var body = "";
+        var req = http.request(options, function(res){
+          res.on('data',function(chunk){
+            body += chunk;
+          });
+          res.on('end',function(){
+            assert.notEqual(body.indexOf("No path by that name found."), -1, "False paths should display an error");
+            exec(BINPATH + " remove receiver somerecv -m test", function(err, stdout){
+              if(err) return next(err);
+              next();
+            });
           });
         });
+        req.write("{}");
+        req.end();
       });
     });
   });
@@ -68,12 +90,21 @@ describe('Server', function(){
     it("shound respond to valid receiver and path",function(next){
       exec(BINPATH + ' add receiver testing localhost -p 8010 -m test', function(err, stderr){
         exec(BINPATH + ' add path testing somepath some/path -m test', function(err, stderr){
-          exec("curl -d '{}' http://localhost:"+config.port+"/testing/somepath", function(err, stdout){
-            assert.notEqual(stdout.indexOf("Job saved. Good job!"), -1, "Valid receivers should display a confirmation.");
-            exec(BINPATH + ' remove receiver testing -m test', function(err, stderr){
-              next();
+          var options = _.extend({path: '/testing/somepath'}, generic_request_options);
+          var body = "";
+          var req = http.request(options, function(res){
+            res.on('data',function(chunk){
+              body += chunk;
+            });
+            res.on('end',function(){
+              assert.notEqual(body.indexOf("Job saved. Good job!"), -1, "Valid receivers should display a confirmation.");
+              exec(BINPATH + ' remove receiver testing -m test', function(err, stderr){
+                next();
+              });
             });
           });
+          req.write("{}");
+          req.end();
         });
       });
     });
@@ -96,9 +127,18 @@ describe('Server', function(){
       endpoint.post('/some/path', function(req, res){
         next();
       });
-      exec("curl -d '{}' http://localhost:"+config.port+"/testing/somepath", function(err, stdout){
-        assert.notEqual(stdout.indexOf("Job saved. Good job!"), -1, "Valid receivers should display a confirmation.");
+      var options = _.extend({path: '/testing/somepath'}, generic_request_options);
+      var body = "";
+      var req = http.request(options, function(res){
+        res.on('data',function(chunk){
+          body += chunk;
+        });
+        res.on('end',function(){
+          assert.notEqual(body.indexOf("Job saved. Good job!"), -1, "Valid receivers should display a confirmation.");
+        });
       });
+      req.write("{}");
+      req.end();
     });
   });
 
@@ -130,12 +170,7 @@ describe('Server', function(){
 
       exec(BINPATH + ' add receiver testoverflow localhost -p 8010 -c 5 -m test', function(){
         exec(BINPATH + ' add path testoverflow someoverflow some/overflow -m test', function(){
-          var options = {
-            host: 'localhost',
-            port: config.port,
-            path: '/testoverflow/someoverflow',
-            method: 'POST'
-          };
+          var options = _.extend({path: '/testoverflow/someoverflow'}, generic_request_options);
           for(var x = 0; x < total_requests; x++){
             var req = http.request(options, function(){});
             req.write("{}");
